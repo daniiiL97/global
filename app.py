b08c7740e7b09b59ddfe32d09c5982403d591cc6
daniiiL97 <151668018+daniiiL97@users.noreply.github.com>
2024-04-17 19:25:26 +0300

    app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import torch
 import re
 
 # Загрузка модели и токенизатора с использованием кеширования
-@st.cache(allow_output_mutation=False)
+@st.cache_resource
 def load_model():
     return GPT2LMHeadModel.from_pretrained("danik97/global-generator-ai")
 
-@st.cache_data(allow_output_mutation=False)
+@st.cache_resource
 def load_tokenizer():
     return GPT2Tokenizer.from_pretrained("danik97/global-generator-ai")
 
